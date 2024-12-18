1bb233145b95ea62ae0e3e28b824f900a46fd763
Rafael Macedo Rosa <143812403+RosaRM@users.noreply.github.com>
2024-12-18 11:04:48 -0300

    criação do código do concat

--- resoluções/concat_dados.py
+++ resoluções/concat_dados.py
@@ -1,1 +1,13 @@
 # Vamos recever dois dados diferentes do usuario. e concatenalas em uma String
+
+# Solicitar o primeiro dado do usuário
+dado1 = input("Digite o primeiro dado: ")
+
+# Solicitar o segundo dado do usuário
+dado2 = input("Digite o segundo dado: ")
+
+# Concatenar os dois dados
+resultado = dado1 + dado2
+
+# Exibir o resultado
+print("A concatenação dos dados é:", resultado)
